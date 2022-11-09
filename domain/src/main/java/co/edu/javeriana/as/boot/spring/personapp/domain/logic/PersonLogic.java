42b5be1c3220527f47c40644b9bde491af2a9fd0
David Ramirez <ramirezmonroyd@gmail.com>
2022-11-09 17:19:57 -0500

    Update Person and Profession logic

--- domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/PersonLogic.java
+++ domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/PersonLogic.java
@@ -4,10 +4,19 @@
  */
 package co.edu.javeriana.as.boot.spring.personapp.domain.logic;
 
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Person;
+
+import java.util.List;
+
 /**
  *
  * @author aasanchez
  */
 public interface PersonLogic {
-    
+    public List<Person> findAll();
+    public void create(Person person);
+    public void delete(Person person);
+    public void deleteById(Person person);
+    public void update(Person person);
+    public Integer count();
 }
--- domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/ProfessionLogic.java
+++ domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/ProfessionLogic.java
@@ -1,4 +1,14 @@
 package co.edu.javeriana.as.boot.spring.personapp.domain.logic;
 
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Profession;
+
+import java.util.List;
+
 public interface ProfessionLogic {
+    public List<Profession> findAll();
+    public void create(Profession profession);
+    public void delete(Profession profession);
+    public void deleteById(Profession profession);
+    public void update(Profession profession);
+    public Integer count();
 }
