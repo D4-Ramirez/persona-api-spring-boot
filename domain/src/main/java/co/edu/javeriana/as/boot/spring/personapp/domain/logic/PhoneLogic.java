a724a38fdb4a1a2ea0fcf0c635b268b26693ef8d
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-11-09 17:23:46 -0500

    Change in StudyLogic and PhoneLogic

--- domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/PhoneLogic.java
+++ domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/PhoneLogic.java
@@ -1,4 +1,17 @@
 package co.edu.javeriana.as.boot.spring.personapp.domain.logic;
 
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Person;
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Phone;
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Profession;
+import co.edu.javeriana.as.boot.spring.personapp.domain.model.Study;
+
+import java.util.List;
+
 public interface PhoneLogic {
+    public List<Phone> findAll();
+    public Phone findByPerson(Person person);
+    public void create(Phone phone);
+    public void delete(String number);
+    public void update(Phone phone);
+    public int count();
 }
--- domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/StudyLogic.java
+++ domain/src/main/java/co/edu/javeriana/as/boot/spring/personapp/domain/logic/StudyLogic.java
@@ -10,8 +10,12 @@ public interface StudyLogic {
     public List<Study> findAll();
     public Study findByPerson(Person person);
     public Study findByProfession(Profession profession);
+
+    public Study findById(Profession profession, Person person);
+
     public void create(Study study);
-    public void delete(Study study);
-    public void deleteById(Person person);
-    public void update(Study study);
+    public void delete(Profession profession, Person person);
+    public void deleteByPerson(Person person);
+    public void deleteByProfession(Profession profession);
+    public void update(Profession profession, Person person, Study study);
 }
