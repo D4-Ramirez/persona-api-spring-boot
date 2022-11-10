5631532a1a5057eacf45d0902ae138fe35ff5762
David Ramirez <ramirezmonroyd@gmail.com>
2022-11-10 12:22:00 -0500

    Create person delete controller

---
+++ rest/src/main/java/co/edu/javeriana/as/boot/spring/personapp/rest/controller/person/PersonDeleteController.java
@@ -0,0 +1,30 @@
+package co.edu.javeriana.as.boot.spring.personapp.rest.controller.person;
+
+import co.edu.javeriana.as.boot.spring.personapp.rest.adapter.PersonaAppAdapter;
+import co.edu.javeriana.as.boot.spring.personapp.rest.mapper.PersonRestMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/person")
+@Tag(name="Person", description = "Person REST API")
+public class PersonDeleteController {
+    @Autowired
+    private PersonaAppAdapter adapter;
+
+    @Operation(summary = "Delete a person", description = "Delete a person that's already in the system", tags = {"Person"})
+    @DeleteMapping(value = "/delete/{id}")
+    @CrossOrigin("*")
+    public ResponseEntity<Boolean> exceute(@PathVariable(value = "id") Integer id) {
+        boolean res = adapter.delete(id, 1);
+        if (res) {
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(false);
+        }
+    }
+}
